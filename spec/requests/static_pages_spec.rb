f19ba8392aaffda343e63b71dfde65042a26ce83
starsmda <darren.starsmore@hispeed.ch>
2012-11-28 13:58:08 +0100

    Adds contacts page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -33,15 +33,30 @@ describe "Static pages" do
 
   describe "About Page" do
 
-  	it "should have the h1 'About Us'" do
-  		visit '/static_pages/about'
-  		page.should have_selector('h1', :text => 'About Us')
-  	end
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
 
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title',
-      			:text => "Ruby on Rails Tutorial Sample App | About Us")
+            :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+            :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
 end
