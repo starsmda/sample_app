960018f8f361a35149c8a857ba54d484218f5fcb
starsmda <darren.starsmore@hispeed.ch>
2012-12-03 18:59:09 +0100

    More customisations

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -22,7 +22,7 @@ describe "Static pages" do
     click_link "Home"
     click_link "Sign up now!"
     page.should have_selector 'title', text: full_title('Sign up')
-    click_link "sample app"
+    click_link "My First Rails App"
     page.should have_selector 'title', text: full_title('')
   end
 
